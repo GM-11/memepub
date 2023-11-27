046f3f4c0ce76f55f844bcdd5a240c72f876a80f
GM-11 <mathuyrgopal@gmail.com>
2023-11-27 19:39:20 +0530

    next.js and hardhat initialized

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+  },
+  paths: {
+    artifacts: "./artifacts"
+  }
+
+};
+
+export default config;
